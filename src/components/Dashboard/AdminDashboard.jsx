1f437e8313782fe79be1e6937df69c82e40eb288
Satyajit Samal <samalsatyajit668@gmail.com>
2024-12-10 14:28:36 +0530

    Chore: wip - Add All Task with style and format

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Header from "../other/Header";
+import CreateTask from "../other/CreateTask";
+import AllTask from "../other/AllTask";
 
 const AdminDashboard = () => {
-    return(
-        <div className="h-screen w-full p-10" >
-            <Header />
-
-            <div>
-                <form>
-                    <h3>Task Title</h3>
-                    <input type="text" placeholder="Make a UI design" />
-                    <h3>Description</h3>
-                    <textarea name="" id="" rows="10"></textarea>
-                </form>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-screen w-full p-7">
+      <Header />
+      <CreateTask />
+      <AllTask />
+    </div>
+  );
+};
 
 export default AdminDashboard;
