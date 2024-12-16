f1feda75453f1364d39a46745e2fb3e365e6d75f
Satyajit Samal <samalsatyajit668@gmail.com>
2024-12-16 09:15:59 +0530

    Chore: wip -  setting up who is logged in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,47 @@ import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [loggedInUserData, setLoggedInUserData] = useState(null);
+  const authData = useContext(AuthContext);
+  // console.log(authData.employees)
+
+  // useEffect(() => {
+  //   const loggedInUser = localStorage.getItem("loggedInUser");
+  //   if (loggedInUser) setUser(loggedInUser.role);
+  // }, [authData]);
 
   const handleLogin = (email, password) => {
-    if(email == 'admin@me.com' && password == '123')
-      setUser('admin')
-    else if(email == 'user@me.com' && password == '123')
-      setUser('employee')
-    else
-      alert("Invalid Credentials")
-  }
+    if (email == "admin@me.com" && password == "123") {
+      setUser({ role: "admin" });
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (authData) {
+      const employee = authData.employees.find(
+        (e) => email == e.email && password == e.password
+      );
+      if (employee) {
+        setUser({ role: "employee" });
+        setLoggedInUserData(employee);
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "employee" })
+        );
+      }
+    } else alert("Invalid Credentials");
+  };
 
   // useEffect(() => {
   //   setLocalStorage();
   //   getLocalStorage();
   // });
 
-  const data = useContext(AuthContext)
-
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
-      {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
+      {user == "admin" ? (
+        <AdminDashboard />
+      ) : user == "employee" ? (
+        <EmployeeDashboard data={loggedInUserData} />
+      ) : null}
     </>
   );
 };
