5df09c8d8b481401e123ea69672af098695c910d
Satyajit Samal <samalsatyajit668@gmail.com>
2024-12-16 09:45:04 +0530

    Chore: wip - add Accept task to task list

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import AcceptTask from "./AcceptTask";
 
-const TaskList = () => {
+const TaskList = ({data}) => {
   return (
     <div
       id="tasklist"
-      className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10 "
+      className="h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10 "
     >
+      <AcceptTask />
       <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl ">
         <div className="flex justify-between items-center">
           <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
